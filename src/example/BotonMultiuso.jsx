4c7a032100b86eb2f14eb4aaec240cc50be1f2ed
Bartender <opascualetti21@gmail.com>
2025-08-05 06:56:25 -0300

    creando botones

---
+++ src/example/BotonMultiuso.jsx
@@ -0,0 +1,17 @@
+
+const BotonMultiuso = (props) => {
+
+    const styles = {
+        
+        backgroundColor: props.color,
+
+        color: 'black',
+        border: 'none'
+    }
+
+    return (
+        <button style={styles}>Click!</button>
+    )
+}
+
+export default BotonMultiuso
